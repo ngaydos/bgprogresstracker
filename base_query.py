9d97c4734a857d3858ac06f0c519d5aa328f9f8b
Nicholas Gaydos <ngaydos@gmail.com>
2018-04-18 14:40:04 -0700

    created total_count and played_count

--- base_query.py
+++ base_query.py
@@ -3,3 +3,11 @@ import psycopg2
 conn = psycopg2.connect('dbname = boardgames user = postgres')
 cur = conn.cursor()
 
+cur.execute("SELECT COUNT(name) FROM games;")
+game_count =  cur.fetchone()[0]
+
+cur.execute("SELECT COUNT(name) FROM games WHERE played_2018 = True;")
+played_count = cur.fetchone()[0]
+
+print game_count
+print played_count
