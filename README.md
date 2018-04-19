e35bf6831dfd5ef3d43ff2a5aa5a30b48c1f18df
Nicholas Gaydos <ngaydos@gmail.com>
2018-04-19 15:04:12 -0700

    update readme + script working

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # bgprogresstracker
-This is a repo for holding the code that will eventually calculate progress on Nick's 2018 board game completion challenge and be able to send text messages with that information.
+
+As a part of maintaing my not insignificant board game collection I have set a challenge to play every game in my collection over the course of 2018. As board games are not exclusively a one person hobby it is important to share this information with the group of people invested in the challenge. This project determines the current rate of progress by checking how much of the year has passes, compares it to the amount of completed board games in a SQL database and sends a message to a group chat via GroupMe providing a status update.
--- groupme_update.py
+++ groupme_update.py
@@ -6,12 +6,14 @@ from base_query import game_count
 from base_query import progress
 
 client = groupy.Client.from_token(groupme_key)
+groups = list(client.groups.list_all())
 
 if ratio >= 1:
     progress_message = ("Progress on Nick's Board Game Challenge Update: Currently, the progress ratio is {} and we are on track to finish by year end").format(round(ratio, 3))
 else:
     progress_message = ("Progress on Nick's Board Game Challenge Update: Currently, the progress ratio is {} at this rate {} games will be played out of {}").format(round(ratio, 3), (round(ratio * game_count, 0)), game_count)
 
-for group in groups:
-    if group.name == 'Board Game Chats':
-        group.post(progress_message)
+if __name__ == '__main__':
+    for group in groups:
+        if group.name == 'Board Game Chats':
+            group.post(progress_message)
