0339c9651a16bcd5b8410795649e7d567f882a91
Nicholas Gaydos <ngaydos@gmail.com>
2018-04-19 13:07:59 -0700

    basic message working

--- groupme_update.py
+++ groupme_update.py
@@ -5,3 +5,13 @@ from datework import year_progress
 from base_query import game_count
 from base_query import progress
 
+client = groupy.Client.from_token(groupme_key)
+
+if ratio >= 1:
+    progress_message = ("Progress on Nick's Board Game Challenge Update: Currently, the progress ratio is {} and we are on track to finish by year end").format(round(ratio, 3))
+else:
+    progress_message = ("Progress on Nick's Board Game Challenge Update: Currently, the progress ratio is {} at this rate {} games will be played out of {}").format(round(ratio, 3), (round(ratio * game_count, 0)), game_count)
+
+for group in groups:
+    if group.name == 'Board Game Chats':
+        group.post(progress_message)
