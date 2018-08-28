4fddffe78bfe3e299df8dd32394059eaf528f667
Nicholas Gaydos <ngaydos@gmail.com>
2018-08-28 00:28:12 -0700

    created review form

--- flask_app/app/forms.py
+++ flask_app/app/forms.py
@@ -6,7 +6,18 @@ from wtforms import TextField, IntegerField, TextAreaField, SubmitField, RadioFi
 class GameForm(FlaskForm):
     conn = psycopg2.connect('dbname = boardgames user = postgres')
     cur = conn.cursor()
-    cur.execute('SELECT * FROM games')
+    cur.execute('SELECT * FROM games ORDER BY name')
     games = [(item[0], item[0]) for item in cur.fetchall()]
     game_select = SelectField('Game Name', choices = games)
     submit =  SubmitField('Select')
+
+class ReviewForm(FlaskForm):
+    conn = psycopg2.connect('dbname = boardgames user = postgres')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM games ORDER BY name')
+    games = [(item[0], item[0]) for item in cur.fetchall()]
+    cur.execute('SELECT * FROM people_index ORDER BY name')
+    people = [(item[0], item[0]) for item in cur.fetchall()]
+    person = SelectField('Person', choices = people)
+    game = SelectField('Game', choices = games)
+    rating = IntegerField('Rating')
