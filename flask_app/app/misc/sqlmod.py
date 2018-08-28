27ad3b0c3109b7e0b6e54302b2bfff61f9dec5c1
Nicholas Gaydos <ngaydos@gmail.com>
2018-08-28 01:00:35 -0700

    updated sqlmod

--- flask_app/app/misc/sqlmod.py
+++ flask_app/app/misc/sqlmod.py
@@ -16,6 +16,9 @@ def insert_rating(game, person, rating):
     #insert a rating into the narrow rating table
     conn = psycopg2.connect('dbname = boardgames user = postgres')
     cur = conn.cursor()
+    cur.execute("SELECT COUNT('rating ID') FROM narrow_ratings WHERE game_id = (SELECT id FROM games WHERE name = '{}') AND person_id = (SELECT id FROM people_index WHERE name = '{}');").format(game, person)
+    if cur.fetchall()[0]:
+        update_rating(game, person, rating)
     cur.execute(("INSERT INTO narrow_ratings (game_id, person_id, rating) VALUES ((SELECT id FROM games WHERE name = '{}'), (SELECT id FROM people_index WHERE name = '{}'), '{}'); ").format(game, person, rating))
     conn.commit()
     conn.close()
@@ -28,9 +31,10 @@ def insert_ratings(game, people, ratings):
     conn = psycopg2.connect('dbname = boardgames user = postgres')
     for value in range(len(people)):
         cur = conn.cursor()
-        cur.execute(("SELECT COUNT('rating_id') FROM narrow_ratings WHERE game_id = (SELECT id FROM games WHERE name = '{}') AND person_id = (SELECT id FROM people_index WHERE name = '{}')").format(game, people[value]))
+        cur.execute(("SELECT COUNT('rating_id') FROM narrow_ratings WHERE game_id = (SELECT id FROM games WHERE name = '{}') AND person_id = (SELECT id FROM people_index WHERE name = '{}');").format(game, people[value]))
         if cur.fetchall()[0][0]:
-            update_rating(game, person, rating)
+            update_rating(game, people[value], ratings[value])
+            continue
         cur.execute(("INSERT INTO narrow_ratings (game_id, person_id, rating) VALUES ((SELECT id FROM games WHERE name = '{}'), (SELECT id FROM people_index WHERE name = '{}'), '{}'); ").format(game, people[value], ratings[value]))
         conn.commit()
     conn.close()
