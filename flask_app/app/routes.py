b809961b6aefbee25fe539217b42f904c3e6f036
Nicholas Gaydos <ngaydos@gmail.com>
2018-08-27 20:45:45 -0700

    updated to try and create submit

--- flask_app/app/routes.py
+++ flask_app/app/routes.py
@@ -20,3 +20,16 @@ def collection():
     cur.execute('SELECT * FROM games')
     games = [item[0] for item in cur.fetchall()]
     return render_template('collection.html', title = 'Collection', games = games)
+
+@app.route('/game/<gamename>')
+def game(gamename):
+    conn = psycopg2.connect('dbname = boardgames user = postgres')
+    cur = conn.cursor()
+    cur.execute(("SELECT * FROM games WHERE name = '{}'").format(gamename))
+    gameinfo = cur.fetchall()[0]
+    name = gameinfo[0]
+    min_players = gameinfo[1]
+    max_players = gameinfo
+    return render_template('game.html', name = gameinfo[0], 
+        min_players = gameinfo[1], max_players = gameinfo[2], best_count = gameinfo[3],
+         type = gameinfo[4], duration= gameinfo[5], played_2018 = gameinfo[6])
