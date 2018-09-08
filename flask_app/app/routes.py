0a14f81f6bb393649a48a6d269b91068b5b7bca5
Nicholas Gaydos <ngaydos@gmail.com>
2018-09-07 22:51:01 -0700

    updated routes to adjust error

--- flask_app/app/routes.py
+++ flask_app/app/routes.py
@@ -84,7 +84,7 @@ def newgame():
     conn = psycopg2.connect('dbname = boardgames user = postgres')
     cur = conn.cursor()
     form = NewGameForm()
-    if request.method = 'POST':
+    if request.method == 'POST':
         cur.execute(("INSERT INTO games(name, min_players, max_players, duration, played_2018) VALUES({}, {}, {}, {}, {})").format
             (form.game_name.data, form.min_players.data, form.max_players.data, form.duration.data, form.played_2018.data))
         if form.best_count != 'None':
